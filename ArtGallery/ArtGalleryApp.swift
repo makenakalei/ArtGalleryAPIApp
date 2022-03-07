8e033f2ac9911e9f7cf69fca7c2128879e7b4d74
Makenakalei <robisonmakena@gmail.com>
2022-03-07 10:22:39 -0800

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Makena Robison on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
